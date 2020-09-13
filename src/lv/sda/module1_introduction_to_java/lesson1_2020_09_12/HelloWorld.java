4b49bf30aaf0e64acd947b12d053afc7660bbda0
Aleksandrs Kalinins <alexvk1984@gmail.com>
2020-09-13 11:50:36 +0300

    Add packages

--- src/lv/sda/module1_introduction_to_java/lesson1_2020_09_12/HelloWorld.java
+++ src/lv/sda/module1_introduction_to_java/lesson1_2020_09_12/HelloWorld.java
@@ -1,11 +1,11 @@
-package lesson1_2020_09_12;
+package lv.sda.module1_introduction_to_java.lesson1_2020_09_12;
 
 //Print Hello World in one line.
 
 public class HelloWorld {
     public static void main(String...args) {
         System.out.print("Hello ");
-        System.out.print( "World");
+        System.out.print("World");
         System.out.print("!");
     }
 }

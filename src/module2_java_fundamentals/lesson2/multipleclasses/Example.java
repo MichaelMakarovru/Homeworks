a666b35abe7a565831745993b00cda265a0b47ab
MichaelMakarovru <71178175+MichaelMakarovru@users.noreply.github.com>
2020-10-17 19:57:39 +0300

    MyHomeworks

--- src/module2_java_fundamentals/lesson2/multipleclasses/Example.java
+++
@@ -1,9 +0,0 @@
-package module2_java_fundamentals.lesson2.multipleclasses;
-
-class Example {
-
-    public static void main(String[] args) {
-        Dog dog = new Dog();
-        Puppy puppy = new Puppy();
-    }
-}
--- src/module2_java_fundamentals/lesson2/oop/Client.java
+++
@@ -1,8 +0,0 @@
-package module2_java_fundamentals.lesson2.oop;
-
-class Client {
-
-    public String name;
-    public int age;
-    public String address;
-}

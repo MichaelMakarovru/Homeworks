cfa5db0c982778223a8d46ab2d11c80327ca6e1d
Aleksandrs Kalinins <alexvk1984@gmail.com>
2020-10-17 11:16:12 +0300

    Git. Lesson 1.

---
+++ src/module3_git/lesson1/Commands.java
@@ -0,0 +1,24 @@
+package module3_git.lesson1;
+
+class Commands {
+    /*
+        git init - command to initialize empty git repository. Run inside your
+                    project's folder.
+
+        git config --global user.name "Ivan Ivanov" - configure user name
+        git config --global user.email "email@gmail.com" - configure user email
+        git config --global --list - list global configs
+
+        git status - display current status of repo.
+        git add <file_name> - add file to tracked
+        git commit -m "Commit message" - create commit
+        git remote add <remote_repo_name, usually origin> <url> - add remote repo
+        git push <remote_repo_name, usually origin> master - push commit to remote repo
+
+        git checkout -b <branch_name> - create new local branch and switch
+        git checkout <branch_name> - switch to other local branch
+
+        git pull <remote_repo_name> <remote_repo_branch_name> - pull
+            changes from remote repo
+     */
+}
